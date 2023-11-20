c3a7d85f5d3c2bb428a09a9f457bf1738a278f42
Steeve ORTUANI <3000937+ripso@users.noreply.github.com>
2023-11-20 10:00:56 +0100

    Fix definitions (#5)
    * Fix definitions
    
    * build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,5 @@
 export type StringObject = Record<string, string>;
 export declare const get: (url: URL | string, params?: StringObject | null, headers?: StringObject) => Promise<Response>;
-export declare const post: (url: URL | string, params: any, headers?: StringObject) => Promise<Response>;
-export declare const put: (url: URL | string, params: any, headers: StringObject) => Promise<Response>;
-export declare const del: (url: URL | string, headers: StringObject) => Promise<Response>;
+export declare const post: (url: URL | string, params?: any, headers?: StringObject) => Promise<Response>;
+export declare const put: (url: URL | string, params?: any, headers?: StringObject) => Promise<Response>;
+export declare const del: (url: URL | string, headers?: StringObject) => Promise<Response>;
